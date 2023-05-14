458e3e6ba601db602e54aa7f394729e31f8af0cb
simonen <si.petrov@yahoo.com>
2023-05-14 15:38:35 +0300

    initial

--- 03.  Tuples and Sets/01.  Tuples and Sets - Lab/06. Summation Pairs.py
+++ 03.  Tuples and Sets/01.  Tuples and Sets - Lab/06. Summation Pairs.py
@@ -1,13 +1,17 @@
 import time
 
-digits = set(input().split())
-target = int(input())
+digits = list(range(10000))
+target = 2500
 start_time = time.time()
-
+count = 0
 while digits:
     a = digits.pop()
     for i in digits:
         if int(i) + int(a) == target:
             print(f"{a} + {i} = {target}")
+            count += 1
 
-print("--- %s seconds ---" % (time.time() - start_time))
+
+end = time.time()
+print(f"Time: {end-start_time}")
+print(count)
