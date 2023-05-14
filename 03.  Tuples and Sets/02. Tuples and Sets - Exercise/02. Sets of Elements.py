b8c8859eabdb1e7afa96a2a10a40938507661a0f
simonen <si.petrov@yahoo.com>
2023-05-14 16:19:58 +0300

    initial

---
+++ 03.  Tuples and Sets/02. Tuples and Sets - Exercise/02. Sets of Elements.py
@@ -0,0 +1,15 @@
+m = set()
+n = set()
+
+first, second = list(map(int, input().split()))
+for _ in range(first):
+    m.add(int(input()))
+for _ in range(second):
+    n.add(int(input()))
+
+[print(i) for i in m.intersection(n)]
+# print(m.intersection(n))
+# print('union:', m.union(n))
+# print('intersection:', m.intersection(n))
+# print('difference:', m.difference(n))
+# print('symmetric difference:', m.symmetric_difference(n))
