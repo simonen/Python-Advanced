6e7bf99ba390b79467b87641b82e6191992c7a39
simonen <si.petrov@yahoo.com>
2023-05-20 13:26:56 +0300

    initial

--- 04. Multidimensional Lists/02. Multidimensional Lists - Exercise 1/03. 2x2 Squares in Matrix.py
+++ 04. Multidimensional Lists/02. Multidimensional Lists - Exercise 1/03. 2x2 Squares in Matrix.py
@@ -7,7 +7,7 @@ for i in range(rows - 1):
     for j in range(cols - 1):
         top_pair = matrix[i][j:j + 2]
         bottom_pair = matrix[i + 1][j:j + 2]
-        if len(set(top_pair).union(set(bottom_pair))) == 1 :
+        if len(set(top_pair).union(set(bottom_pair))) == 1:
             matched_cols.append([top_pair, bottom_pair])
 
 print(len(matched_cols))
