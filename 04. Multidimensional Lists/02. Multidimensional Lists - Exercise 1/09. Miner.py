2654ecb361baf7c37e7b7ea05b3056286ceba766
simonen <si.petrov@yahoo.com>
2023-05-21 18:22:17 +0300

    initial

--- 04. Multidimensional Lists/02. Multidimensional Lists - Exercise 1/09. Miner.py
+++ 04. Multidimensional Lists/02. Multidimensional Lists - Exercise 1/09. Miner.py
@@ -11,15 +11,8 @@ commands_cols = {
     'right': lambda x: x + 1 if x + 1 in range(size) else x
 }
 
-miner = ''
-coal_left = 0
-
-for i in range(size):
-    for j in range(size):
-        if matrix[i][j] == "s":
-            miner = [i, j]
-        elif matrix[i][j] == "c":
-            coal_left += 1
+coal = len([y for x in range(size) for y in range(size) if matrix[x][y] == "c"])
+miner = next(([x, y] for x in range(size) for y in range(size) if matrix[x][y] == "s"))
 
 while commands:
     action = commands.pop(0)
@@ -27,14 +20,15 @@ while commands:
         miner[0] = commands_rows[action](miner[0])
     elif action in commands_cols:
         miner[1] = commands_cols[action](miner[1])
-    if matrix[miner[0]][miner[1]] == "e":
+    row, col = miner
+    if matrix[row][col] == "e":
         print('Game over!', tuple(miner))
         break
-    if matrix[miner[0]][miner[1]] == "c":
-        matrix[miner[0]][miner[1]] = "*"
-        coal_left -= 1
-    if coal_left == 0:
+    if matrix[row][col] == "c":
+        matrix[row][col] = "*"
+        coal -= 1
+    if coal == 0:
         print("You collected all coal!", tuple(miner))
         break
 else:
-    print(f"{coal_left} pieces of coal left. {tuple(miner)}")
+    print(f"{coal} pieces of coal left. {tuple(miner)}")
