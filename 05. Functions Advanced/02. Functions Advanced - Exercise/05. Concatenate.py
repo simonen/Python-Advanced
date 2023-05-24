873ee5e7cd42b8263b98691e41056f991607a75b
simonen <si.petrov@yahoo.com>
2023-05-24 19:45:04 +0300

    initial

---
+++ 05. Functions Advanced/02. Functions Advanced - Exercise/05. Concatenate.py
@@ -0,0 +1,12 @@
+def concatenate(*args, **string):
+    args = "".join(list(args))
+    for key in string:
+        if key in args:
+            args = args.replace(key, string[key])
+    return "".join(args)
+
+
+# print(concatenate("Soft", "UNI", "Is", "Grate", "!",
+# UNI="Uni", Grate="Great"))
+
+print(concatenate("I", " ", "Love", " ", "Cythons", C="P", s="", java='Java'))
