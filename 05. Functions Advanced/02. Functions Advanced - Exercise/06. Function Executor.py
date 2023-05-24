73260e1314d05c63c72111679deacd5eb33c0348
simonen <si.petrov@yahoo.com>
2023-05-24 21:38:04 +0300

    initial

--- 05. Functions Advanced/02. Functions Advanced - Exercise/06. Function Executor.py
+++ 05. Functions Advanced/02. Functions Advanced - Exercise/06. Function Executor.py
@@ -16,15 +16,18 @@ def multiply_numbers(num1, num2):
     return num1 * num2
 
 
-print(func_executor((sum_numbers, (1, 2)), (multiply_numbers, (2, 4)),))
-
 def make_upper(*strings):
- result = tuple(s.upper() for s in strings)
- return result
+    result = tuple(s.upper() for s in strings)
+    return result
+
+
 def make_lower(*strings):
- result = tuple(s.lower() for s in strings)
- return result
+    result = tuple(s.lower() for s in strings)
+    return result
+
+
+print(func_executor((sum_numbers, (1, 2)), (multiply_numbers, (2, 4)), ))
 print(func_executor(
- (make_upper, ("Python", "softUni")),
- (make_lower, ("PyThOn",)),
+    (make_upper, ("Python", "softUni")),
+    (make_lower, ("PyThOn",)),
 ))
