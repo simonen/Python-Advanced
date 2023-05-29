b92a2a00cf8c3de2ab2230c0d47df9a955e0ad0a
simonen <si.petrov@yahoo.com>
2023-05-29 23:51:16 +0300

    initial

--- Python Advanced - Exams/01. Energy Drinks.py
+++ Python Advanced - Exams/01. Energy Drinks.py
@@ -1,24 +1,23 @@
 caffeine = list(map(int, input().split(", ")))
 drinks = list(map(int, input().split(", ")))
 
-stamat = 0
+total_intake = 0
+max_intake = 300
 
 while caffeine and drinks:
     caffe = caffeine.pop()
     drink = drinks.pop(0)
-    caff_amount = caffe * drink
-    if caff_amount <= 300 - stamat:
-        stamat += caff_amount
+    intake = caffe * drink
+    if intake <= max_intake - total_intake:
+        total_intake += intake
     else:
         drinks.append(drink)
-        if stamat >= 30:
-            stamat -= 30
-        else:
-            stamat = 0
+        if total_intake >= 30:
+            total_intake -= 30
 
 if drinks:
     print(f"Drinks left: {', '.join(map(str, drinks))}")
 else:
     print("At least Stamat wasn't exceeding the maximum caffeine.")
 
-print(f"Stamat is going to sleep with {stamat} mg caffeine.")
+print(f"Stamat is going to sleep with {total_intake} mg caffeine.")
