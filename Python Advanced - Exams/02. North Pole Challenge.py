cd282e37925fa8e60e7c1d3a30de848079211ce1
simonen <si.petrov@yahoo.com>
2023-06-10 00:39:58 +0300

    initial

--- Python Advanced - Exams/02. North Pole Challenge.py
+++ Python Advanced - Exams/02. North Pole Challenge.py
@@ -9,21 +9,21 @@ movement = {
 }
 
 items = [matrix[i][j] for i in range(rows) for j in range(cols) if matrix[i][j] not in ['.', 'Y']]
+santa_x, santa_y = next([i, j] for i in range(rows) for j in range(cols) if matrix[i][j] == 'Y')
 decorations = 0
 gifts = 0
 cookies = 0
 is_christmas = False
-santa_x, santa_y = next([i, j] for i in range(rows) for j in range(cols) if matrix[i][j] == 'Y')
+
 command = input()
 
 while command != 'End':
     command = command.split("-")
-    direction = command[0]
-    step = int(command[1])
+    direction, step = command[0], int(command[1])
+
     for i in range(step):
         matrix[santa_x][santa_y] = "x"
         santa_x, santa_y = movement[direction](santa_x, santa_y)
-
         if matrix[santa_x][santa_y] == "D":
             decorations += 1
         elif matrix[santa_x][santa_y] == "G":
