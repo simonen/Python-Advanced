3eb13ac5e939c77bc1d7c93b9bdc5379d9c449c2
simonen <si.petrov@yahoo.com>
2023-06-11 17:37:05 +0300

    initial

--- Python Advanced - Exams/01. Scheduling.py
+++ Python Advanced - Exams/01. Scheduling.py
@@ -2,19 +2,14 @@ jobs = list(map(int, input().split(", ")))
 index = int(input())
 
 cycles = 0
-is_done = False
 
-while jobs:
+while True:
     job_index = jobs.index(min(jobs))
     job = jobs[job_index]
-
-    if index == job_index:
-        is_done = True
 
     cycles += job
     jobs[job_index] = 1000000
 
-    if is_done:
+    if index == job_index:
+        print(cycles)
         break
-
-print(cycles)
