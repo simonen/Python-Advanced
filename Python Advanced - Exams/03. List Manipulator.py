ead56a334b46710be27496dc396f07f1c28da587
simonen <si.petrov@yahoo.com>
2023-06-12 02:03:05 +0300

    initial

---
+++ Python Advanced - Exams/03. List Manipulator.py
@@ -0,0 +1,39 @@
+def list_manipulator(numbers, command, position, *nums):
+    res = numbers
+    if command == 'add' and position == "beginning":
+        res = list(nums) + res
+    elif command == "add" and position == "end":
+        res.extend(nums)
+
+    elif command == "remove" and position == "beginning":
+        if len(nums) == 1:
+            begin = nums[0]
+            res = res[begin:]
+        elif not nums:
+            res.pop(0)
+
+    elif command == "remove" and position == "end":
+        if len(nums) == 1:
+            begin = nums[0]
+            res = res[:-begin]
+        elif not nums:
+            res.pop()
+
+    return res
+
+
+print(list_manipulator([1, 2, 3], "remove", "end"))
+print('---------')
+print(list_manipulator([1, 2, 3], "remove", "beginning"))
+print('---------')
+print(list_manipulator([1, 2, 3], "add", "beginning", 20))
+print('---------')
+print(list_manipulator([1, 2, 3], "add", "end", 30))
+print('---------')
+print(list_manipulator([1, 2, 3], "remove", "end", 2))
+print('---------')
+print(list_manipulator([1, 2, 3], "remove", "beginning", 2))
+print('---------')
+print(list_manipulator([1, 2, 3], "add", "beginning", 20, 30, 40))
+print('---------')
+print(list_manipulator([1, 2, 3], "add", "end", 30, 40, 50))
