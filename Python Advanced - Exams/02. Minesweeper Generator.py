8c3981bb3444a566456190b03eb32fae6a31120c
simonen <si.petrov@yahoo.com>
2023-06-11 23:59:15 +0300

    initial

--- Python Advanced - Exams/02. Minesweeper Generator.py
+++ Python Advanced - Exams/02. Minesweeper Generator.py
@@ -1,6 +1,5 @@
 size = int(input())
-bombs = int(input())
-bomb_pos = [tuple(map(int, input().strip("()").split(", "))) for _ in range(bombs)]
+bomb_pos = [tuple(map(int, input().strip("()").split(", "))) for _ in range(int(input()))]
 matrix = [['-' for j in range(size)] for i in range(size)]
 
 movement = {
@@ -14,17 +13,15 @@ movement = {
     'down_right': lambda x, y: (x + 1, y + 1)
 }
 
-for mine in bomb_pos:
-    b_x, b_y = mine
-    matrix[b_x][b_y] = '*'
-
 for i in range(size):
     for j in range(size):
         near_bombs = 0
-        if matrix[i][j] != '*':
+        if (i, j) in bomb_pos:
+            matrix[i][j] = '*'
+        else:
             for direction in movement:
                 p_x, p_y = movement[direction](i, j)
-                if p_x in range(size) and p_y in range(size) and matrix[p_x][p_y] == "*":
+                if p_x in range(size) and p_y in range(size) and (p_x, p_y) in bomb_pos:
                     near_bombs += 1
 
             matrix[i][j] = str(near_bombs)
