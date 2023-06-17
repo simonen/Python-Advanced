98dc298066a5d396dac32efe98e17da26f264a70
simonen <si.petrov@yahoo.com>
2023-06-17 22:08:41 +0300

    v3

--- Python Advanced - Exams/01. Final Exam/02. Mouse In The Kitchen.py
+++ Python Advanced - Exams/01. Final Exam/02. Mouse In The Kitchen.py
@@ -3,7 +3,6 @@ matrix = [[j for j in list(input())] for i in range(rows)]
 
 mouse_x, mouse_y = next([i, j] for j in range(cols) for i in range(rows) if matrix[i][j] == 'M')
 cheese = [matrix[i][j] for i in range(rows) for j in range(cols) if matrix[i][j] == 'C'].count('C')
-is_trapped = False
 movement = {
     'up': lambda x, y: (x - 1, y),
     'down': lambda x, y: (x + 1, y),
@@ -11,37 +10,36 @@ movement = {
     'right': lambda x, y: (x, y + 1)
 }
 
-direction = input()
+while True:
+    direction = input()
+    if direction == 'danger':
+        print("Mouse will come back later!")
+        break
 
-while direction != "danger":
     p_x, p_y = movement[direction](mouse_x, mouse_y)
 
     if p_x not in range(rows) or p_y not in range(cols):
         print("No more cheese for tonight!")
         break
 
-    if matrix[p_x][p_y] == 'T':
-        is_trapped = True
+    next_pos = matrix[p_x][p_y]
 
-    elif matrix[p_x][p_y] == 'C':
-        cheese -= 1
+    if next_pos == '@':
+        continue
 
-    if matrix[p_x][p_y] != '@':
-        matrix[mouse_x][mouse_y] = '*'
-        matrix[p_x][p_y] = 'M'
-        mouse_x, mouse_y = p_x, p_y
+    matrix[mouse_x][mouse_y] = '*'
+    matrix[p_x][p_y] = 'M'
+    mouse_x, mouse_y = p_x, p_y
 
-    if is_trapped:
+    if next_pos == 'T':
         print("Mouse is trapped!")
         break
+    elif next_pos == 'C':
+        cheese -= 1
 
     if cheese == 0:
         print("Happy mouse! All the cheese is eaten, good night!")
         break
-
-    direction = input()
-else:
-    print("Mouse will come back later!")
 
 for i in matrix:
     print(*i, sep="")
