e6308e76303df4be75d2cb25cac2d14bf58426c1
simonen <si.petrov@yahoo.com>
2023-06-17 20:04:02 +0300

    initial

--- Python Advanced - Exams/03. Words Sorting.py
+++ Python Advanced - Exams/03. Words Sorting.py
@@ -1,9 +1,5 @@
 def words_sorting(*words):
-    book = {}
-    for word in words:
-        if word not in book:
-            book[word] = 0
-        book[word] += sum(ord(i) for i in word)
+    book = {word: sum(map(ord, word)) for word in words}
 
     sorted_book = sorted(book.items(), key=lambda x: -x[1] if sum(book.values()) % 2 != 0 else x[0])
     res = [f'{word} - {value}' for word, value in sorted_book]
