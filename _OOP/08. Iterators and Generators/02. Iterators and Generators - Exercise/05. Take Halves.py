a946449ce6727e5dba667d240a87fe3b46d45c79
simonen <si.petrov@yahoo.com>
2023-07-19 00:26:00 +0300

    initial

---
+++ _OOP/08. Iterators and Generators/02. Iterators and Generators - Exercise/05. Take Halves.py
@@ -0,0 +1,33 @@
+def solution():
+    def integers():
+        start = 1
+        while True:
+            yield start
+            start += 1
+
+    # TODO: Implement
+    def halves():
+        for i in integers():
+            yield i / 2
+
+    # TODO: Implement
+    def take(n, seq):
+        count = 0
+        result = []
+        for item in seq:
+            if count >= n:
+                break
+            result.append(item)
+            count += 1
+        return result
+
+    return (take, halves, integers)
+
+
+take = solution()[0]
+halves = solution()[1]
+print(type(halves()))
+print(type(take(5, [])))
+print(take(5, halves()))
+
+print(list(map(lambda x: x.__name__, solution())))
