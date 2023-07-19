0ee15bc0931894f1ff84bf6831d2d401570d38c1
simonen <si.petrov@yahoo.com>
2023-07-19 12:29:39 +0300

    initial

--- _OOP/08. Iterators and Generators/02. Iterators and Generators - Exercise/02. Dictionary Iterator.py
+++ _OOP/08. Iterators and Generators/02. Iterators and Generators - Exercise/02. Dictionary Iterator.py
@@ -2,19 +2,19 @@ class dictionary_iter:
 
     def __init__(self, dictionary: dict):
         self.dictionary = dictionary
-        self.start = 0
+        self.index = -1
         self.end = len(dictionary)
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.start < self.end:
-            i = self.start
-            self.start += 1
-            return list(zip(self.dictionary.keys(), self.dictionary.values()))[i]
-        else:
+        if self.index == self.end - 1:
             raise StopIteration
+
+        self.index += 1
+
+        return list(self.dictionary.items())[self.index]
 
 
 result = dictionary_iter({1: "1", 2: "2"})
