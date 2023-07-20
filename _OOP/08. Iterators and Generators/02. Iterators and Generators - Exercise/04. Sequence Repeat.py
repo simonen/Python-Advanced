6e7431469bd5bec68ee870bd929381b055010073
simonen <si.petrov@yahoo.com>
2023-07-20 11:36:42 +0300

    initial

--- _OOP/08. Iterators and Generators/02. Iterators and Generators - Exercise/04. Sequence Repeat.py
+++ _OOP/08. Iterators and Generators/02. Iterators and Generators - Exercise/04. Sequence Repeat.py
@@ -1,32 +1,26 @@
-from math import ceil
-
-
 class sequence_repeat:
 
     def __init__(self, sequence: str, number: int):
         self.sequence = sequence
         self.number = number
-        self.start = 0
-        self.ext_seq = ceil(self.number / len(sequence)) * self.sequence
+        self.index = -1
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.start < self.number:
-            i = self.start
-            self.start += 1
-            return self.ext_seq[i]
-        else:
+        if self.index == self.number - 1:
             raise StopIteration
 
+        self.index += 1
+
+        return self.sequence[self.index % len(self.sequence)]
 
 
-result = sequence_repeat('abc', 5)
-print(result.ext_seq)
-# for item in result:
-#  print(item, end ='')
-
-result = sequence_repeat('I Love Python', 3)
+result = sequence_repeat('abc', 7)
 for item in result:
- print(item, end='')
+    print(item, end='')
+
+# result = sequence_repeat('I Love Python', 3)
+# for item in result:
+#     print(item, end='')
