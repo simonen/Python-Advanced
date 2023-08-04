b5a15d2332a7478e10cfd6b3658c20d485a1d493
simonen <si.petrov@yahoo.com>
2023-08-04 23:30:01 +0300

    initial

--- _OOP/Exams/Exam - 10 April 2022/project/supply/drink.py
+++ _OOP/Exams/Exam - 10 April 2022/project/supply/drink.py
@@ -3,8 +3,8 @@ from project.supply.supply import Supply
 
 class Drink(Supply):
 
-    def __init__(self, name: str, energy=15):
-        super().__init__(name, energy)
+    def __init__(self, name: str):
+        super().__init__(name, 15)
 
     def details(self) -> str:
         return f"Drink: {self.name}, {self.energy}"
